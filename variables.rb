457e05949ddb14870a8aeaf1623c8723850f019c
Nick Tassinari <ntass7800@gmail.com>
2023-02-05 16:33:59 -0700

    add puts statements

--- variables.rb
+++ variables.rb
@@ -1,7 +1,7 @@
 phrase1 = "For lunch I would like "
 phrase2 = "a nice Chana Masala"
 phrase3 = "to eat "
-full_phrase = phrase1 + phrase3 + phrase2
+
 
 fav_num = 3
 emergency = 911
@@ -11,4 +11,13 @@ makeout = 6.4
 
 reality = false  
 vacant_brain = true
+
+phrase1 = "For dinner I would like"
+phrase2 = "a spicy chicken sandwich!"
+phrase3 = " to devour "
+
+full_phrase = phrase1 + phrase3 + phrase2
+puts full_phrase 
+
+puts "If you think that is #{vacant_brain} this must be #{reality}"
 
